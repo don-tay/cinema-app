a6348a3b49f62ca05a22f554836123d2cc76cb17
Don Tay <dontay0209@gmail.com>
2022-10-30 22:48:34 +0800

    Add logic to send email after confirmation

--- php/src/confirm.php
+++ php/src/confirm.php
@@ -46,39 +46,45 @@ $seatIdsStr = implode(",", $seatIds);
           echo "One or more seats are already ticketed";
           
       } else {
-        // otherwise, create a new ticket for each seat
-        foreach ($seatIds as $seatId) {
-            $sql = "START TRANSACTION;";
-            $result = $conn->query($sql);
-            $sql = "INSERT INTO tickets (email, seat_id) VALUES ('" . $fields["email"] . "', " . $seatId . ")";
-            $result = $conn->query($sql);
-            $sql = "COMMIT;";
-            $result = $conn->query($sql);
-        }
-
-        // output redirect to homepage button
         if ($seatIdsStr !== '') {
-          $sql = "SELECT s.seat_id, s.seat_num, m.title, ss.start_time FROM seats s JOIN showings ss ON (s.showing_id = ss.showing_id) JOIN movies m ON (ss.movie_id = m.movie_id) WHERE s.seat_id IN (" . $seatIdsStr . ")";
-          $result = $conn->query($sql);
-          if ($result->num_rows > 0) {
-            echo "Your confirmed seats are:";
-            // output data of each row
-            while($row = $result->fetch_assoc()) {
-                echo "<div id='seat-" . $row['seat_id'] . "' class='checkout-item'>";
-                echo "<div class='checkout-item-title'>";
-                echo "<h3>" . $row['title'] . "</h3>";
-                echo "</div>";
-                echo "<div class='checkout-item-content'>";
-                echo "<p>Seat: " . $row['seat_num'] . "</p>";
-                echo "<p>Time: " . $row['start_time'] . "</p>";
-                echo "</div>";
-                echo "</div>";
+            $sql = "INSERT INTO tickets (seat_id, email) VALUES " . implode(',', array_map(function ($seatId) use ($fields) {
+            return "($seatId, '{$fields['email']}')";
+            }, $seatIds));
+            if ($conn->query($sql) === TRUE) {
+                $sql = "SELECT s.seat_id, s.seat_num, m.title, ss.start_time FROM seats s JOIN showings ss ON (s.showing_id = ss.showing_id) JOIN movies m ON (ss.movie_id = m.movie_id) WHERE s.seat_id IN (" . $seatIdsStr . ")";
+                $result = $conn->query($sql);
+                if ($result->num_rows > 0) {
+                    echo "Your confirmed seats are:";
+                    $email_str = "Your confirmed seats are:\n\n";
+                    // output data of each row
+                    while($row = $result->fetch_assoc()) {
+                        echo "<div id='seat-" . $row['seat_id'] . "' class='checkout-item'>";
+                        echo "<div class='checkout-item-title'>";
+                        echo "<h3>" . $row['title'] . "</h3>";
+                        echo "</div>";
+                        echo "<div class='checkout-item-content'>";
+                        echo "<p>Seat: " . $row['seat_num'] . "</p>";
+                        echo "<p>Time: " . $row['start_time'] . "</p>";
+                        echo "</div>";
+                        echo "</div>";
+                        $email_str .= $row['title'] . " Seat " . $row['seat_num'] . "\n";
+                    }
+                    $email_str .= "\n\nThank you for booking with Pacific Cinema! We hope you enjoy the show!";
+                    $email_header = "From: Pacific Cinema < pacific-cinema@mail.hellodon.dev >";
+                    $status = mail($fields['email'], "Pacific Cinema Ticket Confirmation", $email_str, $email_header);
+                    if ($status) {
+                        echo "Email sent successfully";
+                    } else {
+                        echo "Email failed to send";
+                    }
+                } else {
+                echo "Error: " . $sql . "<br>" . $conn->error;
+                }
             }
-          }
         }
       }
       // output redirect to homepage button
-      echo "<button onclick=\"window.history.go(-1)\">Return to homepage</button>";
+      echo "<button onclick=\"window.location.href='index.php'\">Return to homepage</button>";
       ?>
   </body>
 
